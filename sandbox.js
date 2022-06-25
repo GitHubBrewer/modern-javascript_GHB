bdb5e3dd8e030b2249a10770ad0a494f56a3407a
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-06-25 11:18:36 -0500

    chapter5

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+alert('hello, world');
+/* In JS you have statments
+alert('hello, world') is a statement
+Semi-Colons are used to denote the end of a statement
+Most of the time you don't need to denote the end of a statement
+However, adding the semi-colon helps to eliminate that as a possible casue during troubleshooting
+ */
