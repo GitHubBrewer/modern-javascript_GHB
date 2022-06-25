d8caafc964944ef825dae08bd72c4272fbe51c11
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-06-25 17:28:57 -0500

    chapter11

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,42 @@
-// common string methods
+let radius = 10;
+const pi = 3.14;
 
-let email = 'mario@thenetninja.co.uk';
+// console.log(radius, pi);
 
-// let result = email.lastIndexOf('n');
+// math operators +, -, *, /, **, %
 
-// let result = email.slice(0,10);
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius**2;
 
-// let result = email.substring(4,10);
+// order of operations B I D M A S
 
-// let result = email.replace('m', 'w');
+// let result = 5 * (10 - 3)**2;
 
-let result = email.replace('n', 'w');
+// console.log(result);
 
+let likes = 10;
+console.log(likes);
+// likes = likes + 1;
+// shorthand notation
+// increases by 1
+likes++;
+console.log(likes);
+// decreases by 1
+likes--;
+console.log(likes);
+// adds that value
+likes += 10;
+console.log(likes);
+// subtracts that value
+likes -= 5;
+console.log(likes);
+// multiplies that value
+likes *= 2;
+console.log(likes);
+// divides that value
+likes /=2;
+console.log(likes);
+
+let result = 'the blog has ' + likes + ' likes';
 console.log(result);
