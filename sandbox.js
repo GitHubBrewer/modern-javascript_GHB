25a9452d70de081d7612c30ecd07af7f478e1a9f
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-06-25 12:44:57 -0500

    chapter7

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,17 @@
 alert('hello, world');
 console.log(1);
+
+// modern variables are set using 2 methods
+// let sets a starting value for a variable but can be updated
+let age = 25;
+console.log(age);
+
+age = 30;
+console.log(age);
+
+// const sets a static variable and is not allowed to be updated
+const year = 1981;
+console.log(year);
+
+year = 2022;
+console.log(year);
