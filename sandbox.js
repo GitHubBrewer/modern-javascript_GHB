16fc65c901ba38459dfb178987375fee10c481b3
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-06-26 16:49:46 -0500

    chapter12

--- sandbox.js
+++ sandbox.js
@@ -1,42 +1,21 @@
-let radius = 10;
-const pi = 3.14;
+// template strings
+const title = 'Best reads of 2022';
+const author = 'Mario';
+const likes = 30;
 
-// console.log(radius, pi);
-
-// math operators +, -, *, /, **, %
-
-// console.log(10 / 2);
-// let result = radius % 3;
-// let result = pi * radius**2;
-
-// order of operations B I D M A S
-
-// let result = 5 * (10 - 3)**2;
-
+// concatentation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 // console.log(result);
 
-let likes = 10;
-console.log(likes);
-// likes = likes + 1;
-// shorthand notation
-// increases by 1
-likes++;
-console.log(likes);
-// decreases by 1
-likes--;
-console.log(likes);
-// adds that value
-likes += 10;
-console.log(likes);
-// subtracts that value
-likes -= 5;
-console.log(likes);
-// multiplies that value
-likes *= 2;
-console.log(likes);
-// divides that value
-likes /=2;
-console.log(likes);
-
-let result = 'the blog has ' + likes + ' likes';
+// template string way
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
 console.log(result);
+
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html); 
