7995a955f998ec114598319c0d1247290a474158
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-06-26 17:20:43 -0500

    chapter13

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,46 @@
-// template strings
-const title = 'Best reads of 2022';
-const author = 'Mario';
-const likes = 30;
+// arrays
+// part of the OBJECT data type
+// denoted by elements contained within square brackets
+// separate elements with commas
+let ninjas = ['shaun','ryu','chun-li'];
+// can isolate specific elements with square-bracket notation
+// JS is still zero-based language
+console.log(ninjas[1]);
 
-// concatentation way
-// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
-// console.log(result);
+ninjas[1] = 'ken';
+console.log(ninjas[1]);
 
-// template string way
-let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// arrays can hold other data types as well
+let ages = [20,25,30,35];
+console.log(ages[1]);
+
+// arrays can also hold mixed data types
+let random = ['shaun','crystal',20,30];
+console.log(random);
+
+// arrays also have properties like the other data types
+console.log(ninjas.length);
+
+// array methods
+let result = ninjas.join(',');
 console.log(result);
 
-// creating html templates
-let html = `
-    <h2>${title}</h2>
-    <p>By ${author}</p>
-    <span>This blog has ${likes} likes</span>
-`;
+// join defaults to comma if nothing is passed in
+result = ninjas.join();
+console.log(result);
 
-console.log(html); 
+result = ninjas.indexOf('chun-li');
+console.log(result);
+
+result = ninjas.concat(['ken','crystal']);
+console.log(result);
+
+// push is a destructive method and resturns the length of the array
+result = ninjas.push('ken');
+console.log(result);
+console.log(ninjas);
+
+// pop is also destructive and returns the last item removed
+result = ninjas.pop();
+console.log(result);
+console.log(ninjas);
