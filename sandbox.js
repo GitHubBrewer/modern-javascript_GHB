aad7125dbe8ed8f0c5da2abb99e5210bf0a1c0f9
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-04 14:13:27 -0500

    chapter26

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,20 @@
-// logical NOT (!)
-// placing an ! infront of a boolean vairable will return the opposite
+// break and continue
+// break is used to leave the loop when a conditional statement is TRUE
+// continue skips the rest of the code block and starts the next iteration when a coditional statement is TRUE
 
-let user = false;
+const scores = [50, 25, 0, 30, 100, 20, 10];
 
-if (!user) {
-    console.log('you must be logged in to continue')
+for (let i = 0; i < scores.length; i++) {
+    
+    if(scores[i] === 0){
+        continue;
+    }
+
+    console.log(`your score: ${scores[i]}`);
+
+    if(scores[i] === 100){
+        console.log('congratulations, you got the top score!');
+        break;
+    }
+    
 }
-
-console.log(true);
-console.log(!true);
-console.log(false);
-console.log(!false);
