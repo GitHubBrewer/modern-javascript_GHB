a8ff930e28f0ee66578353ea7991cf8b2997530a
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-04 10:00:51 -0500

    chapter23

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,51 @@
-// if statements
-// these differe from loops in that they are only meant to run once
-
-let age = 20;
-
-if (age > 20) {
-    console.log('you are over 20 years old');
-}
-
-age = 21;
-if (age > 20) {
-    console.log('you are over 20 years old');
-}
-
-const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-if (ninjas.length > 3) {
-    console.log(`There are ${ninjas.length} ninjas. That's a lot of ninjas!`);
-}
-
-// we can use if statements to check things like password length or complexity
-const password = 'pass';
+// else if statements
+// we'll start with an else statement
+let password = 'p@ss';
 
 if (password.length >= 8) {
     console.log('that password is long enough');
+} else {
+    console.log(`Your password is only ${password.length} characters long. That's too short!`);
 }
-// unfortunately this only provides feedback in one state
-// we can tell you if it's long enough OR too short, but not both
+
+// here it is if the password meets the first condition
+password = 'p@ssword';
+
+if (password.length >= 8) {
+    console.log('that password is long enough');
+} else {
+    console.log(`Your password is only ${password.length} characters long. That's too short!`);
+}
+
+// if we want to check more than two conditions we can use else if statements
+password = 'p@ss';
+
+if (password.length >= 12) {
+    console.log(`Your password is mighty strong since it's ${password.length} characters long!`)
+} else if (password.length >= 8){
+    console.log('that password is long enough, but could be stronger');
+} else {
+    console.log(`Your password is only ${password.length} characters long. That's too short!`);
+}
+
+// testing with longer password to meet 2 statement
+password = 'p@ssword';
+
+if (password.length >= 12) {
+    console.log(`Your password is mighty strong since it's ${password.length} characters long!`)
+} else if (password.length >= 8){
+    console.log('that password is long enough, but could be stronger');
+} else {
+    console.log(`Your password is only ${password.length} characters long. That's too short!`);
+}
+
+// testing with even longer password to meet first condition
+password = 'p@ssword1234';
+
+if (password.length >= 12) {
+    console.log(`Your password is mighty strong since it's ${password.length} characters long!`)
+} else if (password.length >= 8){
+    console.log('that password is long enough, but could be stronger');
+} else {
+    console.log(`Your password is only ${password.length} characters long. That's too short!`);
+}
