182ae4223b6f2c0bb70a37da447ef9e45cc4d665
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-05 11:33:52 -0500

    chapter30

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,32 @@
-// variables and block level scope
-// variable set with global scope
-let age = 30;
+// functions can be created two different ways
+    // they can be declared or they can be expressed
+// declared functions are hoisted by JS meaning they are read as though they were at the top of the file
+// this allows them to be called/invoked before actually declaring them
+greet();
+greet();
+greet();
 
-// let age = 50;
+// expressed functions are not hoisted and so must be expressed before they are invoked/called
+// comment out the call below to see the rest of the code execute
+speak();
+// speak();
+// speak();
 
-if (true) {
-    // local scope for age and can only be accessed inside this code block
-   let age = 40;
-   let name = 'shaun'
-   console.log(`inside 1st code block age = ${age} and name = ${name}`);
-    if (true) {
-        let age = 50;
-        console.log(`inside 2nd code block age = ${age} and name = ${name}`);
-    }
-
+// function declaration
+// we start with the function statement and then name the function
+// we can pass in (optional variables) and follow it with the {code block} 
+function greet() {
+    console.log('hello there');
 }
-// we can't access variables created inside code blocks
-console.log(`outside code block age = ${age} and name = ${name}`);
+
+// function expression
+// we start with a const to prevent changing the function by mistake
+// we name the function then follow the same process as the declaration with one addition
+// expressions MUST end with the ; after the code block since we are expressing it through a variable
+const speak = function () {
+  console.log('good day');
+};
+
+speak();
+speak();
+speak();
