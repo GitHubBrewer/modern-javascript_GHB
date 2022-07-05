7113b86f4524f914dd3d005c03391455323d2dd8
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-05 11:04:04 -0500

    withVar

--- sandbox.js
+++ sandbox.js
@@ -7,13 +7,19 @@ let age = 30;
 if (true) {
     // local scope for age and can only be accessed inside this code block
    let age = 40;
-   let name = 'shaun'
-   console.log(`inside 1st code block age = ${age} and name = ${name}`);
+   const name = 'shaun'
+   console.log(`inside 1st code block age = ${age} and name = ${name} and test = ${test}`);
     if (true) {
+        // because this code block is inside another code block we can initialize an existing variable again because of local scope
+        // global and block/level scope works for const and let
         let age = 50;
-        console.log(`inside 2nd code block age = ${age} and name = ${name}`);
+        const name = 'mario';
+        // the older var usage ignores block/level scope and variable initialized with it are available outside their local scope
+        var test = 'hello';
+        console.log(`inside 2nd code block age = ${age} and name = ${name} and test = ${test}`);
     }
 
 }
-// we can't access variables created inside code blocks
-console.log(`outside code block age = ${age} and name = ${name}`);
+// we can't access variables created inside code blocks as seen with the name variable
+// unless they are defined with the older var statement
+console.log(`outside code block age = ${age} and name = ${name} and test = ${test}`);
