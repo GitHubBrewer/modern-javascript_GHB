63b037c27f9778b65fb0f65d948b897817a0c075
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-10 14:38:18 -0500

    chapter33

--- sandbox.js
+++ sandbox.js
@@ -1,43 +1,55 @@
-// returning values
-// begin with a function expression with a parameter of radius
-const calcAreaLogged = function(radius) {
-    // define variable with the calculation of a circle's area when it's radius is passed in
-    let areaLogged = 3.14 * radius**2;
-    // now we can log the result to the console
-    console.log(areaLogged);
-};
-
-// here we call the function and pass in an argument of 5 for the radius
-// we can see the result in the console
-calcAreaLogged(5);
-// logging it to the console in the code block above doesn't let us do anything with areaVerbose later
-// areaVerbose is a local scope variable that can't be used outside the code block
-// console.log(areaLogged); //un-comment this line to see
-
-// instead we can return the area as part of the function so it can be used
-const calcAreaReturned = function(radius) {
-    let areaReturned = 3.14 * radius**2;
-    return areaReturned;
-};
-// we can now call the function but event though the value is returned we need to do a little more work to show that
-calcAreaReturned(6);
-// we need to store the value in memory somewhere by storing it in a variable
-const areaReturned = calcAreaReturned(6);
-console.log(areaReturned);
-
-// we can clean the function expression up by combining the calculation and return call in one line
-const calcArea = function(radius) {
+// regular function
+const calcAreaOriginal = function(radius) {
     return 3.14 * radius**2;
 };
-// we store the returned value in a variable
-const area = calcArea(7);
-// now we can use the returned value outside the block scope
-console.log(area);
 
-// we can even use it in other functions
-const calcVol = function(area,height) {
-    return area * height;
+const areaOriginal = calcAreaOriginal(5);
+console.log(`the area is ${areaOriginal}`);
+
+// arrow function
+// first we can remove the function keyword and add the arrow
+const calcArea1 = (radius) => {
+    return 3.14 * radius**2;
+};
+const area1 = calcArea1(5);
+console.log(`the area is ${area1}`);
+
+// when there is only one parameter we can remove the ()
+// if there is none or more than one we still keep the ()
+const calcArea2 = radius => {
+    return 3.14 * radius**2;
+};
+const area2 = calcArea2(5);
+console.log(`the area is ${area2}`);
+
+// when there is a simple return line we can remove return and scoot the code block up to the first line
+const calcArea = radius => 3.14 * radius**2;
+
+const area = calcArea(5);
+console.log(`the area is ${area}`);
+
+// practice converting regular to arrow
+const greetReg = function () {
+    return 'hello world';
 };
 
-const vol = calcVol(area,5);
-console.log(vol);
+const greet = () => 'hello world';
+
+const billReg = function(products, tax) {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+         total += products[i] + products[i] * tax;
+    }
+    return total;
+};
+
+console.log(billReg([10,15,30],0.2));
+
+const bill = (products,tax) => {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+};
+console.log(bill([10,15,30],0.2));
