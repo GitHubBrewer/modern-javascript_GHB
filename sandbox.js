5c432d219dae616fecc634e2e1aea0cb3a1e900b
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-10 20:44:47 -0500

    chapter35

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,20 @@
-const name = 'shaun';
+// callback functions
+// this is where you define a function on it's own
+// then you call that function within another function
+// here's an example from MDN that I formatted as arrow functions
+const greeting = name => alert('Hello ' + name);
 
-// functions
-const greet = () => 'hello';
+const processUserInput = callback => {
+  var name = prompt('Please enter your name.');
+  callback(name);
+}
 
-let resultOne = greet();
-console.log(greet());
+processUserInput(greeting);
 
+// here is shaun's example that calls a callback function from inside a method
+// remembering that methods are just function that are called with dot notation
+let people = ['mario', 'luigi', 'ryu', 'chun-li', 'shaun'];
 
-// methods
-let resultTwo = name.toUpperCase();
-console.log(resultTwo);
+const logPerson = people.forEach((person, index) => {
+    console.log(`${index} - hello ${person}`);
+});
