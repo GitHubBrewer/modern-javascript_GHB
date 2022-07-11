183a7777397e10cb0dba26f7b7fbd2292e7f8daa
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-10 21:00:46 -0500

    chapter36

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,13 @@
-// callback functions
-// this is where you define a function on it's own
-// then you call that function within another function
-// here's an example from MDN that I formatted as arrow functions
-const greeting = name => alert('Hello ' + name);
+// get a reference to the ul (unordered list)
+const ul = document.querySelector('.people');
 
-const processUserInput = callback => {
-  var name = prompt('Please enter your name.');
-  callback(name);
-}
+const people = ['mario', 'luigi', 'ryu', 'chun-li', 'shaun'];
 
-processUserInput(greeting);
+let html = ``;
 
-// here is shaun's example that calls a callback function from inside a method
-// remembering that methods are just function that are called with dot notation
-let people = ['mario', 'luigi', 'ryu', 'chun-li', 'shaun'];
-
-const logPerson = people.forEach((person, index) => {
-    console.log(`${index} - hello ${person}`);
+people.forEach(function(person){
+    html += `<li style="color: purple">${person}</li>`;
 });
+
+console.log(html);
+ul.innerHTML = html;
