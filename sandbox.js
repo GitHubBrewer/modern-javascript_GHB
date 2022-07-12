e40123e09fc41f5f03d5b601a0a9320540976cfb
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-11 19:41:56 -0500

    chapter40

--- sandbox.js
+++ sandbox.js
@@ -14,10 +14,37 @@ let user = {
     logout: function(){
         console.log('the user logged out');
     },
+    // we can't access the key:values inside this object directly
+    // we need to use this.key to access them
+    // we can;t use an arrow function for that action though
+    // arrow functions define the this keyword at the time the method is invoked
+    logBlogsArrow: () => {
+    },
     logBlogs: function(){
-
+        console.log(this);
+        console.log(this.blogs);
+        console.log(`${this.name} has written the following blogs:`);
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
+    },
+    // finally we can clean up the functions inside objects by removing the word function and backing it up to the key
+    logBlogsClean(){
+        console.log('this looks better');
+        console.log(`${this.name} has written the following blogs:`);
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
     }
 };
 
 user.login();
 user.logout();
+// this refers to the window object when used at the global scope
+console.log(this);
+// when used inside an object this refers to the object
+// but arrow functions define this based on the level when invoked
+user.logBlogsArrow();
+// regular functions keep this at the level it is coded
+user.logBlogs();
+user.logBlogsClean();
