cc6cfaa4f9c8f7e8e903e7bdb41462ab55883e89
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-11 19:18:51 -0500

    chapter39

--- sandbox.js
+++ sandbox.js
@@ -1,32 +1,23 @@
 // object literals
-// created with {}
-// filled with key:value pairs separated by ,
 
 let user = {
     name: 'crystal',
     age: 30,
-    location: 'berlin',
     email: 'crystal@thenetninja.co.uk',
-    blogs: ['why mac and cheese rules', '10 things to make with marmite']
+    location: 'berlin',
+    blogs: ['why mac and cheese rules', '10 things to make with marmite'],
+    // methods are created as key:value pairs as well
+    // the key is the method name and the value is the function
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    },
+    logBlogs: function(){
+
+    }
 };
 
-console.log(user);
-// values can be accessed by dot notation
-console.log(user.name);
-
-// values can be updated by dot notation
-user.age = 35;
-console.log(user.age);
-
-// values can also be accessed by [] notation
-console.log(user['location']);
-// and updated by [] notation
-user['name'] = 'chun-li';
-console.log(user['name']);
-
-// an advantage to [] notation is the ability to pass in variables
-let key = 'location';
-console.log(user[key]);
-// we couldn't use user.key since JS would be looking for a key:value called key
-
-console.log(typeof user);
+user.login();
+user.logout();
