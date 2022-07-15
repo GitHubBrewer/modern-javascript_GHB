e571f3ffc2d7e1049093deabfb0b5d46ad2786fa
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-15 16:33:26 -0500

    chapter43

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,40 @@
-// Math object
+// primitive vs reference types
+// primitive types are stored in the stack in memory (limited space but fast access)
+    // numbers
+    // strings
+    // Booleans
+    // null
+    // undefined
+    // symbols
+// reference types are stored in the heap in memory (more space but slower access)
+    // all types of objects
+        // object literals
+        // arrays
+        // functions
+        // dates, Math, etc.
+// primitive values
+let scoreOne = 50;
+let scoreTwo = scoreOne;
 
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.E);
+console.log(`scoreOne: ${scoreOne}, scoreTwo: ${scoreTwo}`);
 
-// we can use methods on the Math object
-const area = 7.7;
-// we can round to the nearest whole number
-console.log(Math.round(area));
-// we can round down to the whole number
-console.log(Math.floor(area));
-// we can round up to the whole number
-console.log(Math.ceil(area));
-// we can truncate to the whole number
-console.log(Math.trunc(area));
 
-// we can generate random numbers between 0 and 1
-const random = Math.random();
-console.log(random);
+// because primitive values are stored in the stack they keep their values unless explicitly updated
+// scoreTwo is already set to the intial value scoreOne had (50) and updating scoreOne doesn't update scoreTwo
+scoreOne = 100;
+console.log(`scoreOne: ${scoreOne}, scoreTwo: ${scoreTwo}`);
 
-// if we want a random number between 1 and 100
-console.log(random * 100);
-console.log(Math.round(random * 100));
+// reference values
+// these values are stored on the heap with a pointer from the variable to the values stored in the stack
+const userOne = { name: 'ryu', score: 50};
+// this adds a new record in the stack but points to the same array in the heap, not a second array
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+// because the pointers are stored in the stack and both point to the same array, updating the array updates for both
+userOne.name = 'chun-li';
+console.log(userOne, userTwo);
+
+// it also means that updating userTwo will update userOne
+userTwo.score = 100;
+console.log(userOne, userTwo);
