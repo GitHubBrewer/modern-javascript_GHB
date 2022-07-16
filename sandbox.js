a4b6bdd6c2a95b0596f5926f7b6874e0ad570519
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-16 15:52:00 -0500

    chapter49

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,15 @@
-// now we can start making changes to the dom since we can reference it accurately
-// let's grab the first p tag
-const para = document.querySelector('p');
+// in addition to innerText and innerHTML we can also manipulate attributes
+// here we grab the anchor tag
+const link = document.querySelector('a');
 
-// we want the text inside the tag, not the whole tag
-console.log(para.innerText);
-// now we can change the text or append the text
-para.innerText = 'ninjas are awesome!';
-// para.innerText += 'ninjas are awesome!';
+// now we can get and set Attributes like the href that points the link to a site/file
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk');
+link.innerText = 'the net ninja website';
 
-// or we could cycle through all the p tags and append new text
-const paras = document.querySelectorAll('p');
-paras.forEach(para => {
-    console.log(para.innerText);
-    para.innerText += ' new text';
-});
-
-// we can also change the HTML
-const content = document.querySelector('.content');
-console.log(content.innerHTML);
-// here we are appending to the HTML inside the node that has the content class
-content.innerHTML += '<h2>this is new html</h2>'
-
-// we can also pretend that we are pulling in info from a DB
-const people = ['mario', 'luigi', 'yoshi'];
-
-// now we cycle through each element in the array and append it to the node that has the content class
-people.forEach(person => {
-    content.innerHTML += `<p>${person}</p>`;
-});
+const mssg = document.querySelector('p');
+console.log(mssg.getAttribute('class'));
+// here we update the class from error to success
+mssg.setAttribute('class', 'success');
+// here we insert a new attribute to change the font color
+mssg.setAttribute('style', 'color: green;');
