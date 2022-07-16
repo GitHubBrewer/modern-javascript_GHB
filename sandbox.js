e03fa87afcf620ad8bd1322a8b2b80e8c3177a6f
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-16 17:27:56 -0500

    chapter50

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,21 @@
-// in addition to innerText and innerHTML we can also manipulate attributes
-// here we grab the anchor tag
-const link = document.querySelector('a');
-
-// now we can get and set Attributes like the href that points the link to a site/file
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'https://www.thenetninja.co.uk');
-link.innerText = 'the net ninja website';
-
-const mssg = document.querySelector('p');
-console.log(mssg.getAttribute('class'));
-// here we update the class from error to success
-mssg.setAttribute('class', 'success');
-// here we insert a new attribute to change the font color
-mssg.setAttribute('style', 'color: green;');
+// we've seen how to modify elements and attibutes and how to add addtributes
+// how do we add additional attributes to existing ones
+const title = document.querySelector('h1');
+// when we try to add a margin to the style attribute that already is coloring the text orange
+title.setAttribute('style', 'margin: 50px;'); 
+// this has overwritten the color style attribute. 
+// let's set it back to orange
+title.setAttribute('style', 'color: orange;'); 
+// we can actually access the individual style properties instead
+// here we show all possible CSS style properties
+console.log(title.style);
+// now we grab just the color
+console.log(title.style.color);
+// now we can access just the one we want to update without affecting the others
+title.style.margin = '50px';
+// we can also update them directly
+title.style.color = 'crimson';
+// for style properties that are "double-barrel" like font-size, we remove the - and make it camel-case
+title.style.fontSize = '60px';
+// to remove a style property, set it to an empty string
+title.style.margin = '';
