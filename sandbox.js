023a9d615d55057c8ca9b5d458605717ab905785
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-17 12:28:58 -0500

    chapter52

--- sandbox.js
+++ sandbox.js
@@ -1,35 +1,24 @@
-// lets add and remove classes
-// grab the h2 tag
-const content = document.querySelector('h2');
-// we can get a  DOMTokenList using the classList property
-console.log(content.classList);
-// we can add a class
-content.classList.add('error');
-// remove the class
-content.classList.remove('error');
-// add a different class
-content.classList.add('success');
-// add another class
-content.classList.add('test');
-
-// now lets apply a class based on the text in the p tag
-// grab all the p tags
-const paras = document.querySelectorAll('p');
-
-// now check to see if the text contains error or success and add the corresponding class
-paras.forEach(para => {
-   if (para.innerText.includes('error')) {
-    para.setAttribute('class', 'error');
-   } else if (para.innerText.includes('success')) {
-    para.setAttribute('class', 'success');
-   };
+// if we want to target all the tags under a parent we could target each in a separate cosnt
+// or we could target the parent and use the child property
+const article = document.querySelector('article');
+console.log(article.children);
+// this leaves us with an HTML collection that we can't use forEach on
+// we can convert it into an array using Array.from()
+console.log(Array.from(article.children));
+// this method is non-desctructive
+console.log(article.children);
+Array.from(article.children).forEach(child => {
+    child.classList.add('article-element');
 });
-// could also use para.textContent to grab all text even if set to hidden
-// could use classList.add instead if you want to ensure you don't overwrite other existing classes
 
-// we can also toggle classes on/off with the toggle method
-const title = document.querySelector('.title');
-// checks to see if the class is there and adds/removes based on what it finds
-title.classList.toggle('test');
-
-title.classList.toggle('test');
+// there are many more properties available in node relationships
+const title = document.querySelector('h2');
+// parents is the opposite of children
+console.log(title.parentElement);
+// we can even chain up or down
+console.log(title.parentElement.parentElement);
+// we can move inside between siblings
+console.log(title.nextElementSibling);
+console.log(title.previousElementSibling);
+// and even chain in multiple levels to access elements that we can't get to directly
+console.log(title.nextElementSibling.parentElement.children);
