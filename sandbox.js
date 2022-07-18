73bd0108d9eb3d9176e45552388116d399e339f5
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-18 07:12:46 -0500

    chapter55

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,36 @@
-// we want to look at how to remove elements
+// we need a way to continue listening for events when new elements are added
+// first we need to understand event bubbling
+// i've added logging for when an event is triggered in the li and the ul
+// we find that triggering th li event also triggers the ul event
+// events bubble up through the heirarchy
 const items = document.querySelectorAll('li');
-items.forEach(item => {
-    item.addEventListener('click', (e) => {
-        // we'll use the remove method on the items we're cycling through
-        e.target.remove();
-    });
-});
-// now let's look at adding elements
-// let's grab the unordered list element that we want to add things to
+// commented the belowout to move the listen/action to the parent
+// items.forEach(item => {
+//     item.addEventListener('click', (e) => {
+//         e.target.remove();
+//         console.log('event in LI');
+//         // we can stop the bubbling with the stopPropagation method
+//         e.stopPropagation();
+//     });
+// });
+// this still doesn't solve our issue of not listening to new tags when they're added
+// for this we need event delegation
+// this uses the bubbling to pass the target up to parents where it can be accessed
+// this lets us perform the eventListen at a higher level and receive events from lower levels
+// then we take the target passed up and perform the desired action
 const ul = document.querySelector('ul');
-// also grab the button that we use to trigger the additions
 const button = document.querySelector('button');
-// listen for the button click
 button.addEventListener('click', () => {
-    // option 1 adds to the html directly (only allows append)
-    ul.innerHTML += '<li>option 1 something new</li>'
-    // option 2 creates the element
     const newLi = document.createElement('li');
-    newLi.textContent = 'option 2 something new';
-    // then we decide if we want to append or prepend using a method and passing in the variable we created
+    newLi.textContent = 'something new';
     ul.prepend(newLi);
-    // ul.append(newLi);
+});
+// moving the listen up to the parent
+ul.addEventListener('click', e => {
+    console.log('event in UL');
+    // determining if the event was from the LI
+    if (e.target.tagName === 'LI') {
+        // performing the remove
+        e.target.remove();
+    }
 });
