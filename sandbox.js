51b66c0bfee1254913732a1796c0876ef64db58c
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2022-07-23 14:54:17 -0500

    chapter56

--- sandbox.js
+++ sandbox.js
@@ -1,36 +1,26 @@
-// we need a way to continue listening for events when new elements are added
-// first we need to understand event bubbling
-// i've added logging for when an event is triggered in the li and the ul
-// we find that triggering th li event also triggers the ul event
-// events bubble up through the heirarchy
-const items = document.querySelectorAll('li');
-// commented the belowout to move the listen/action to the parent
-// items.forEach(item => {
-//     item.addEventListener('click', (e) => {
-//         e.target.remove();
-//         console.log('event in LI');
-//         // we can stop the bubbling with the stopPropagation method
-//         e.stopPropagation();
-//     });
-// });
-// this still doesn't solve our issue of not listening to new tags when they're added
-// for this we need event delegation
-// this uses the bubbling to pass the target up to parents where it can be accessed
-// this lets us perform the eventListen at a higher level and receive events from lower levels
-// then we take the target passed up and perform the desired action
-const ul = document.querySelector('ul');
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    const newLi = document.createElement('li');
-    newLi.textContent = 'something new';
-    ul.prepend(newLi);
+// let's look at some other events
+// we'll start wth the copy event that is triggered by highliighting something and right-clicking and selecting copy or using ctrl+c
+// first we'll choose the class we want to focus on
+const copyWarning = document.querySelector('.copy-me');
+// now we add the event listener to it
+copyWarning.addEventListener('copy', () => {
+    // and set a callback function that is triggered by it
+    console.log('oi, my content is copyrighted!');
 });
-// moving the listen up to the parent
-ul.addEventListener('click', e => {
-    console.log('event in UL');
-    // determining if the event was from the LI
-    if (e.target.tagName === 'LI') {
-        // performing the remove
-        e.target.remove();
-    }
+// now we can track the movement of the mouse
+// we'll select the box class
+const box = document.querySelector('.box');
+// we add the listener with the mousemove type to trigger it
+box.addEventListener('mousemove', e => {
+    // show what we can see
+    // console.log(e);
+    // show the values of the mouse offsets within the box class object
+    // console.log(e.offsetX, e.offsetY);
+    // replace the text content with what we want to display in the box
+    box.textContent = `the x position is ${e.offsetX} and y position is ${e.offsetY}`
+});
+// finally we'll look at the wheel event
+document.addEventListener('wheel', e => {
+    // we can log out the position of the mouse relatvie to the visible page
+    console.log(e.pageX, e.pageY);
 });
