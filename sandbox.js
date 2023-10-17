8d72193b7c3d3d4fda342f59c1cc70f8a0afd8c5
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2023-10-17 18:27:13 -0500

    updated comments

--- sandbox.js
+++ sandbox.js
@@ -15,6 +15,7 @@ const names = ['shaun', 'mario', 'luigi'];
 
 for(let i = 0; i < names.length; i++){
     // square brackets are used to denote the index we want
+    // console.log(names[i]);
     // in a string that's a specific character, in an array that's an entire element
     console.log(`the name in position ${i} is ${names[i]}`);
     // we can use the loop to assist in creating some html
