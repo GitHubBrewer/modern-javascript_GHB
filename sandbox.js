32cd7a5433ced5b12dc7a7d8870bc767c5631667
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2023-10-25 21:05:45 -0500

    added section to cycle through an array
    I wanted to try this else if function on an array of passwords
    so I combined it with a while loop.
    In production I would never console out an actual password!

--- sandbox.js
+++ sandbox.js
@@ -15,3 +15,25 @@ if (password.length >= 12 && password.includes('@')) {
 } else {
     console.log(`Your password is only ${password.length} characters long. That's too short!`);
 }
+
+console.log ("Now let's try doing this on an array of passwords");
+// creating an array of passwords
+let passwords = ["pass","password","p@ssword","p@ssword1234"];
+
+i = 0
+
+while (i < passwords.length){
+    if (passwords[i].length >= 12 && passwords[i].includes('@')) {
+        console.log(`"${passwords[i]}" is a mighty strong password!`);
+        i++;
+    } else if (passwords[i].length >= 8 && !passwords[i].includes('@')) {
+        console.log(`"${passwords[i]}" is long enough at ${passwords[i].length} characters, but could be stronger.`);
+        i++;
+    } else if (passwords[i].includes('@') && passwords[i].length > 5) {
+        console.log(`"${passwords[i]}" has the potential to be a mighty strong password, but it's a little short at only ${passwords[i].length} characters.`);
+        i++;
+    } else {
+        console.log(`C'mon, you can do better! "${passwords[i]}" is only ${passwords[i].length} characters long!`);
+        i++;
+    }
+}
