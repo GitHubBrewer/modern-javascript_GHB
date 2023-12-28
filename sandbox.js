5481ac9f8d54e514df69afb0d0c6aad547c0eadc
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2023-12-28 10:15:02 -0600

    improved comments and clarified a function name

--- sandbox.js
+++ sandbox.js
@@ -3,9 +3,9 @@
 // parameters are defined in the function
 // arguments are passed into the function when it's called
 // start with a function expression
-// we add a parameter to the function this time called name
+// we add parameters to the function called name and time
 const speak = function(name, time) {
-    // we can then use the parameter in the code block
+    // we can then use the parameters in the code block
     console.log(`good ${time} ${name}!`);
 };
 
@@ -13,20 +13,21 @@ const speak = function(name, time) {
 speak('mario', 'morning');
 // note that the code parameter can't be called outside the code block as it only has local/block scope
 console.log(name);
+// we just get an empty string
 
 // what happens if we fail to pass in any arguments
 speak();
 
-// to avoid this we can define default values when we establish the parameters
-const speakDefault = function(name = 'luigi', time = 'night') {
+// to avoid these undefined outputs we can define default values when we establish the parameters
+const speakWDefault = function(name = 'luigi', time = 'night') {
     console.log(`good ${time} ${name}`);
 };
 // now we can call the function and receive default values
-speakDefault();
+speakWDefault();
 // or override with my own values
-speakDefault('mario','morning');
+speakWDefault('mario','morning');
 // or only override with one value
-speakDefault('mario');
+speakWDefault('mario');
 // i'm not sure how to only pass in the 2nd argument
-speakDefault('','morning');
+speakWDefault('','morning');
 
