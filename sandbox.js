5d73fb1d3c644ab04c17bcd5be0341f360bd46f7
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2023-12-28 20:40:04 -0600

    orig was dup of 31, created new notes for lesson

--- sandbox.js
+++ sandbox.js
@@ -1,43 +1,57 @@
 // returning values
-// begin with a function expression with a parameter of radius
-const calcAreaLogged = function(radius) {
-    // define variable with the calculation of a circle's area when it's radius is passed in
-    let areaLogged = 3.14 * radius**2;
-    // now we can log the result to the console
-    console.log(areaLogged);
+// we are currently only returning information into the console
+// how do we return a value we can do something with?
+
+// let's start by creating a constant that is a function that calulates the area of a circle
+const wrongCalcArea = function(radius){
+    let wrongArea = 3.14 * radius**2;
+    console.log(wrongArea);
 };
 
-// here we call the function and pass in an argument of 5 for the radius
-// we can see the result in the console
-calcAreaLogged(5);
-// logging it to the console in the code block above doesn't let us do anything with areaVerbose later
-// areaVerbose is a local scope variable that can't be used outside the code block
-// console.log(areaLogged); //un-comment this line to see
+// this lets us return a value, but we can't do anything with it
+wrongCalcArea(5);
+// nothing shows in the console even though we're passing an argument into the function
 
-// instead we can return the area as part of the function so it can be used
-const calcAreaReturned = function(radius) {
-    let areaReturned = 3.14 * radius**2;
-    return areaReturned;
+// this next line breaks the page so uncomment to see what it does, but re-comment to see the rest of the work
+// console.log(wrongArea);
+// wrongArea is a local-scope variable, not global, so we can't use it outside the function
+
+// let's try again and instead of outputing the value into the console
+// use return so we can try and use the value for something
+const fullCalcArea = function(radius){
+    let area = 3.14 * radius**2;
+    return area;
 };
-// we can now call the function but event though the value is returned we need to do a little more work to show that
-calcAreaReturned(6);
-// we need to store the value in memory somewhere by storing it in a variable
-const areaReturned = calcAreaReturned(6);
-console.log(areaReturned);
 
-// we can clean the function expression up by combining the calculation and return call in one line
-const calcArea = function(radius) {
+// now we still can't just grab area since it's still local-scope
+// instead we create a const that calls the function with a parameter we pass in
+const fullArea = fullCalcArea(5);
+console.log(fullArea);
+
+// we can clean up the function a little more
+// we don't need to return the value as a local variable, we can just return the output of the function
+const calcArea = function(radius){
     return 3.14 * radius**2;
 };
-// we store the returned value in a variable
-const area = calcArea(7);
-// now we can use the returned value outside the block scope
+
+// we get the same results as above
+const area = calcArea(5);
 console.log(area);
 
-// we can even use it in other functions
-const calcVol = function(area,height) {
-    return area * height;
+// now we can use the first fuction to do more work, like calculate the volume of a cylinder
+// we can also pass in default values for the parameters like we saw in the last lesson
+const calcVol = function(height = 1){
+  return height * area;  
 };
 
-const vol = calcVol(area,5);
-console.log(vol);
+// returns the value of h * pi * r**2 where r = 5 and h is passed in as 2
+console.log(calcVol(2));
+
+// can I return that information using a template string?
+console.log(`the volume of a cylinder with a radius of 5 and height of ${calcVol.height} is `,calcVol(2));
+
+// can I return any of this in a template string?
+console.log(area.radius);
+calcVol(2);
+console.log(`the volume of a cylinder with a radius of ${calcArea.radius} and height of ${calcVol.height} is `,calcVol(2));
+console.log(calcVol(5));
