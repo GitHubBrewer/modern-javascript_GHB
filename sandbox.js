c2ed435603abd92a3bcaec96ec16dcedf0c0acaf
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2024-03-10 20:46:19 -0500

    added more comments for clarity and context

--- sandbox.js
+++ sandbox.js
@@ -26,10 +26,25 @@ paras.forEach(para => {
 });
 // could also use para.textContent to grab all text even if set to hidden
 // could use classList.add instead if you want to ensure you don't overwrite other existing classes
-
+/*
+paras.forEach(p => {
+    if(p.textContent.includes('error')){
+        p.classList.add('error');
+    }
+    if (p.textContent.includes('success')){
+        p.classList.add('success');
+    }
+});
+*/
 // we can also toggle classes on/off with the toggle method
 const title = document.querySelector('.title');
 // checks to see if the class is there and adds/removes based on what it finds
 title.classList.toggle('test');
 
 title.classList.toggle('test');
+// testing to see what happens if I remove the class that we are using to the grab the element
+title.classList.toggle('title');
+// it removed the class, now what happens if we try to add it back
+title.classList.toggle('title');
+// looks like it must identify the element by something else that allows us to continue to reference it
+// even after removing the thing we used to identify it
