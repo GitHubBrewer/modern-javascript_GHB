fea16b69769bcc24f460b75ba2660bc02773e26c
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2024-03-12 18:00:03 -0500

    added clarifying comments

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,15 @@
+// NODE RELATIONS
+// we can interact with elements/nodes, but we can also interact with nodes through relationships
+// parent/child relationships move up/down the tree
+// elements on the same level (have the same direct parent) are siblings
 // if we want to target all the tags under a parent we could target each in a separate cosnt
 // or we could target the parent and use the child property
+// sometimes we won't know what the children are, as they might be dynamically generated
 const article = document.querySelector('article');
 console.log(article.children);
-// this leaves us with an HTML collection that we can't use forEach on
-// we can convert it into an array using Array.from()
+// this leaves us with an HTML collection
+// we wan't to use forEach on this collection, but we can't
+// we can convert it instead into an array using Array.from()
 console.log(Array.from(article.children));
 // this method is non-desctructive
 console.log(article.children);
