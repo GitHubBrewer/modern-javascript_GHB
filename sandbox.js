3939d6a227afe0747917931c38aeb5d613eae5b5
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2024-03-22 15:38:11 -0500

    intial commit

--- sandbox.js
+++ sandbox.js
@@ -1,76 +1,33 @@
-// sort method
+// chaining array methods
 
-// example 1 - sorting strings
-const namesOne = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
-const namesTwo = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
-const namesThree = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
-
-// sort in alphabetical order
-namesOne.sort();
-console.log(namesOne);
-
-// this is a destructive method even if you store the sorted array in a new const
-const namesStored = namesTwo.sort();
-console.log(namesStored, namesTwo);
-
-// to be non-destructive, we need to use .toSorted()
-const namesSafeSort = namesThree.toSorted();
-console.log(namesSafeSort, namesThree);
-
-
-// example 2 - sorting numbers
-const scores = [10, 50, 20, 5, 35, 70, 45];
-scores.sort();
-// notice how this doesn't actually work? It's converting the numbers to strings
-console.log(scores);
-
-// to correct this we need to use the compare function in the method
-// we will also use toSorted to avoid creating a bunch of arrays for demonstration
-// from https://www.w3schools.com/js/js_array_sort.asp
-// When the sort() function compares two values, it sends the values to the compare function, and sorts the values according to the returned (negative, zero, positive) value.
-// If the result is negative, a is sorted before b. If the result is positive, b is sorted before a. If the result is 0, no changes are done with the sort order of the two values.
-const scoresTrue = scores.toSorted(function(a,b){return a - b});
-console.log(scores, scoresTrue);
-
-
-// example 3 - sorting objects
-const players = [
-    {name: 'mario', score: 20},
-    {name: 'luigi', score: 10},
-    {name: 'chun-li', score: 50},
-    {name: 'yoshi', score: 30},
-    {name: 'shaun', score: 70}
+const products = [
+    {name: 'gold star', price: 30},
+    {name: 'green shell', price: 10},
+    {name: 'red shell', price: 40},
+    {name: 'banana skin', price: 5},
+    {name: 'mushroom', price: 50}
 ];
 
-console.log('original', players);
+// filter out any product that is <= 20 or actually return any product > 20
+const filtered = products.filter(product => product.price > 20 );
+console.log(filtered);
 
-// need to provide additional information for objects because it needs to know what to compare
-// a represents the current item and b is the next item
-const playersOne = players.toSorted((a,b) => {
-    // this is a simplified version of what the compare function is doing
-    if(a.score < b.score){
-        return -1; // when a is lower return a negative number to decrease its position
-    } else if (b.score < a.score){
-        return 1; //when a is higher return a postive number to increase its position
-    } else {
-        return 0; // when they are equal return 0 and don't change positions
-    }
+// then take the filtered array and format the items with a sale price
+const promos = filtered.map(product => {
+    return {name: product.name, price: product.price / 2};
 });
-console.log('sorted ascending by score', playersOne);
+console.log(promos);
 
-// we can also clean up the arrow function and reverse it by swapping the calculation
-const playersTwo = players.toSorted((a,b) => b.score - a.score);
-console.log('sorted descending by score', playersTwo);
+// now let's chain them
+const chainedLong = products.filter(product => product.price > 20 ).map(product => {
+    return {name: product.name, price: product.price / 2};
+});
+console.log(chainedLong);
 
-// bonus method - reverse
-const namesFour = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
-// if we want to reverse then we can sort as normal
-namesFour.sort();
-// then reverse it
-namesFour.reverse();
-console.log(namesFour);
-
-const scoresReverse = [10, 50, 20, 5, 35, 70, 45];
-scoresReverse.sort();
-scoresReverse.reverse();
-console.log(scoresReverse);
+// now we can clean up and format it according to convention where we indent each link in the chain
+const chainedClean = products
+    .filter(product => product.price > 20 )
+    .map(product => {
+        return {name: product.name, price: product.price / 2};
+});
+console.log(chainedClean);
