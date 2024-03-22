4ea29b2fa9178d6049d191f3e60e8653af9b5703
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2024-03-21 19:11:06 -0500

    initial commit

--- sandbox.js
+++ sandbox.js
@@ -1,36 +1,8 @@
-// reduce method
-// returns any single value you want based on the values you iterate over
-const scores = [10, 20, 60, 40, 70, 90, 30];
+// find method
+// returns the first element in an array that passes a test in the callback function
+const scores = [10, 5, 0, 50, 60, 10 ,20, 70];
 
-// use reduce method to return a result of how many scores are over 50
-// takes in two parameters, accumulator and current
-// acc is a running total and is rememberd through each iteration
-// doesn't have to be a value, it could be an object that we add a property to
-// curr is the value of the item in the array that we are currently iterating through
-const result = scores.reduce((acc, curr) => {
-    if(curr > 50){
-        acc++;
-    }
-    // remember to return the acc or the code in the if block doesn't mean anything
-    return acc;
-    // this is where we declare the starting value of acc
-}, 0);
+// find the first score in this array that is over 50 and return that value
+const firstHighScore = scores.find(score => score > 50);
 
-console.log(result);
-
-const complexScores = [
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60}
-];
-
-// we want to use the reduce method to calculate the total score of mario's scores
-const marioResult = complexScores.reduce((acc, curr) => {
-    if(curr.player === 'mario'){
-        acc += curr.score;
-    }
-    return acc;
-}, 0);
-
-console.log(marioResult);
+console.log(firstHighScore);
