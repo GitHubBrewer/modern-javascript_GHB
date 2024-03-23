ff3b24e7e81a99bbb5d3b348d9680619d3c9185e
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2024-03-23 11:16:27 -0500

    chapter79 hooked up the add todo form to add new todos

---
+++ app.js
@@ -0,0 +1,44 @@
+// 1. starting with grabbing the form by grabbing the add class 
+const addForm = document.querySelector('.add');
+// 4. now we need to grab the ul to inject our template string into
+const list = document.querySelector('.todos');
+
+// 3. now we create a function to do something with the value we grab in the addForm
+// we do it outside the eventlistener to allow us to reuse it for other future functionality
+const generateTemplate = todo => {
+
+    // we could inject the element using document.createElement https://www.w3schools.com/jsref/met_document_createelement.asp
+    // but with more complex elements it can be easier to use a template string
+    // here I just grab an existing li and edit the span to insert our new todo
+    const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+    // now we can append it to the end of the ul, but this will add even if the field is blank
+    list.innerHTML += html;
+};
+
+// 2. now we create an event listener on the submit action
+// since the search is not a child of the add form, we don't have to worry about the submit action of search bubbling up to it
+// we will need to address the search's default action of resetting the page later though
+addForm.addEventListener('submit', e => {
+
+    // preventing the page reset on submit
+    e.preventDefault();
+
+    // trim the value to avoid extraneous spaces
+    // assigning the value of the event to be additive
+    const todo = addForm.add.value.trim();
+
+    // before we add the todo, we want to ensure it isn't blank
+        // my idea was simple, but there was a simpler way
+        // if (todo != '')
+    if (todo.length){
+        // using the function I created above to insert the new item
+        generateTemplate(todo);    
+        // but we also want to clear the form after the submit
+        addForm.reset();
+    }
+});
