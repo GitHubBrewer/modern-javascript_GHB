f537c28d681cca71978852af210ad95ee46799ad
GitHubBrewer <44078322+GitHubBrewer@users.noreply.github.com>
2024-03-24 18:47:11 -0500

    intial commit

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,21 @@
-// async in action
-console.log(1);
-console.log(2);
+// making http requests
+// first we create the request object, the XML data type is old and this request can work with many data types
+const request = new XMLHttpRequest();
 
-setTimeout(() => {
-    console.log('the callback function fired');
-}, 2000);
+// 3. we can track the progress of the request if we attach an eventListener to the request
+// we'll track the state changes https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+request.addEventListener('readystatechange', () => {
+    // we can see in the console that it goes through 4 states from the request
+    // console.log(request, request.readyState);
+    // we only really care when it reaches readyState 4 so we can create an if statement for that
+    if (request.readyState === 4) {
+        // then we can grab the responseText
+        console.log(request.responseText);
+    }
+});
 
-console.log(3);
-console.log(4);
+// 1. we create the open request stating the method (GET) and the url
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+// 2. we send the request
+request.send();
+// console.log(request);
